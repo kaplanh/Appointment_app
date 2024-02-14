cfc61983118f423e895d873cbb894186b05ba20f
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-14 19:56:31 +0100

    refactor:App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import HomePage from "./pages/HomePage";
-import "./App.css"
+import "./App.css";
+import Home from "./pages/Home";
+// !react-icons: 1-yarn add react-icons
+//!2- import { AiOutlineDelete } from "react-icons/ai"
 
-
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <HomePage/>
+        <div>
+            <Home />
+            {/*3- <AiOutlineDelete className="text-danger fs-3" /> */}
         </div>
     );
-}
+};
 
 export default App;
